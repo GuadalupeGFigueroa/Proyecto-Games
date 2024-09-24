c2f2f582f77cb669d2a854c996d6fb2b7dcc5b9a
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-09-24 12:34:24 +0200

    Comprobación dentro de rango, avisos de número mayor o menor y contador

--- adivinaNumero.java
+++ adivinaNumero.java
@@ -1,38 +1,52 @@
-import java.util.Scanner;
-// import javax.swing.JOptionPane;
 import java.util.Random;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class adivinaNumero {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        // generamos un numero entero desde 0 a 100
-        Random aleatorio = new Random();
-        int numero = aleatorio.nextInt(101);
+   // generamos un numero entero desde 0 a 100
+   Random aleatorio = new Random();  //crea una instancia de la clase Random, que proporciona métodos para generar números aleatorios. La variable aleatorio es el objeto que utilizarás para acceder a estos métodos.
+   int numero = aleatorio.nextInt(101);  //utiliza el método nextInt(int bound) del objeto aleatorio para generar un número entero aleatorio entre 0 (inclusive) y 101 (exclusivo), es decir, entre 0 y 100. El número generado se almacena en la variable numero
+   int contador = 1;  // lo inicializamos en 1, por si lo acierta a la primera 
+    
 
-        System.out.println("");
+   JOptionPane.showMessageDialog(null,"¡Vamos a jugar! Tienes que adivinar un número entero entre 0 y 100");
 
-        System.out.println("¡Vamos a jugar! Tienes que adivinar un número entero entre 0 y 100 \n\nPrueba y dime un número");
-        int intento = sc.nextInt();
+   int intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));  // nos queda limitar la introducción a este rango
 
-        System.out.println("");
+   while(intento>100 || intento<0) { //comprobamos si el valor esté dentro del rango
+        JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100.");
+        intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
+       
+   }    
+     
+    while(intento != numero) {
+        if(intento < numero) {
+            JOptionPane.showMessageDialog(null, "¡Lo siento! Te has quedado corto, inténtalo de nuevo con un número mayor. \nEste ha sido tu intento número " + contador);
+            intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
+        }
+            else {
+                JOptionPane.showMessageDialog(null, "¡Lo siento! Te has pasado de largo, inténtalo de nuevo con un número menor. \nEste ha sido tu intento número " + contador);                JOptionPane.showMessageDialog(null,"¡Lo siento! Intenta con un número menor.");
+                intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
+            }
+        contador= contador +1;
 
+        while(intento>100 || intento<0) { //comprobamos si el valor esté dentro del rango
+            JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100.");
+            intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
 
-
-
-        sc.close();
-
+        }
     }
+           
+    JOptionPane.showMessageDialog(null, "¡Has acertado! El número aleatorio es: " + numero + "\nY lo has conseguido en tan solo " + contador + " intentos");
+            
+    sc.close();  
+ 
+
+
+}
 }
 
-/*
-        boolean z = random.nextBoolean();
-
-        System.out.println(z);
-        
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name? ");
-        String name = scanner.nextLine();
-
-        */
