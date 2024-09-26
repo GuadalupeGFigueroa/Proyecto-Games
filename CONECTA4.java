8177853e50a6bcd52f76aa1a238a68c3d035c642
GuadalupeGFigueroa <guadalupe.g.figueroa@gmail.com>
2024-09-26 11:21:29 +0200

    Añadido código que comprueba que el valor sea numérico. Contador de todos los casos

---
+++ CONECTA4.java
@@ -0,0 +1,3 @@
+public class CONECTA4 {
+    
+}
--- adivinaNumero.java
+++ adivinaNumero.java
@@ -1,106 +1,73 @@
 import java.util.Random;
-import java.util.Scanner;
-
 import javax.swing.JOptionPane;
-
 public class adivinaNumero {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-    // generamos un numero entero desde 0 a 100
-    Random aleatorio = new Random();  //crea una instancia de la clase Random, que proporciona métodos para generar números aleatorios. La variable aleatorio es el objeto que utilizarás para acceder a estos métodos.
-    int numero = aleatorio.nextInt(101);  //utiliza el método nextInt(int bound) del objeto aleatorio para generar un número entero aleatorio entre 0 (inclusive) y 101 (exclusivo), es decir, entre 0 y 100. El número generado se almacena en la variable numero
-    int contador = 1;  // lo inicializamos en 1, por si lo acierta a la primera
-    
-    // boolean continua;  // para reiniciar el juego o no
-
-    // int entero; // para saber si es un número entero y no una letra
-
-
-// AQUI EMPIEZA EL PROGRAMA Y AQUÍ TENDRÍA QUE VOLVER CUANDO DIGA QUE SÍ QUIERE VOLVER A JUGAR -----------------------
-    JOptionPane.showMessageDialog(null,"¡Vamos a jugar! Tienes que adivinar un número entero entre 0 y 100");
-
-    int intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
-
-    while(intento>100 || intento<0) { //comprobamos si el valor es entero (que no sean letras) y esté dentro del rango
-        JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100.");
-        intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
-    }
-
-    while(intento != numero) {
-        if (intento < numero) {
-            JOptionPane.showMessageDialog(null, "¡Lo siento! Te has quedado corto, inténtalo de nuevo con un número mayor. \nEste ha sido tu intento número " + contador);
-            intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
-        }
-            else {
-                JOptionPane.showMessageDialog(null, "¡Lo siento! Te has pasado de largo, inténtalo de nuevo con un número menor. \nEste ha sido tu intento número " + contador);
-                intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
-            }
-        contador= contador +1;
-
-        while(intento>100 || intento<0) { //comprobamos si el valor esté dentro del rango
-            JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100.");
-            intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
-
-        }
-    }
-
-    
-    // aquí debajo, indica que ha acertado y le pregunta si quiere continuar, eligiendo entre 2 opciones
-    JOptionPane.showMessageDialog(null, "¡Has acertado! El número aleatorio es: " + numero + "\nY lo has conseguido en tan solo " + contador + " intentos");
-
-    Object[] opciones = {"Sí", "No"};
-    int respuesta = JOptionPane.showOptionDialog(null, "¿Quieres volver a jugar?", "Pregunta", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
-    
-    while (intento == numero && respuesta == 0) {  //hace el bucle si el número es acertado y si respondió que sí a seguir jugando
-        if (respuesta == 0) {
-            JOptionPane.showMessageDialog(null, "¡Bien, vamos allá! Tienes que adivinar de nuevo un número entero entre 0 y 100");
-            aleatorio = new Random();  // VOLVEMOS A GENERAR OTRO NUMERO RANDOM
-            numero = aleatorio.nextInt(101); 
-            contador = 1;
-            intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
-
-            while(intento>100 || intento<0) { //comprobamos si el valor es entero (que no sean letras) y esté dentro del rango
-                JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100.");
-                intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
-            }
-
-            while(intento != numero) {
-                if(intento < numero) {
-                    JOptionPane.showMessageDialog(null, "¡Lo siento! Te has quedado corto, inténtalo de nuevo con un número mayor. \nEste ha sido tu intento número " + contador);
-                    intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
-                }
-                    else {
+        // PASO 0. meter número RANDMON e inicilizar variables y contadores
+            // generamos un numero entero desde 0 a 100, que se almacena en "numero"
+        Random aleatorio = new Random();  //crea una instancia de la clase Random, para generar números aleatorios
+        int numero = aleatorio.nextInt(101);  //genera un número entero aleatorio entre 0 (inclusive) y 101 (exclusivo) y lo almacena en la variable numero
+        // contador de intentos del usuario, lo inicializamos en 1, para que cuente el intento si lo acierta a la primera
+        int contador = 1;
+            // 0 = sí, 1 = no
+        int respuesta = 0;
+            // es un contador para ayudarme a seguir el código, es borrable al final *****
+        // int i = 1;
+        boolean arriba = true;  // si reponde sí a seguir, sigue haciendo el bucle
+        // PASO 1. BUCLE PRINCIPAL, mientras la respuesta a seguir jugando sea sí, seguimos en este bucle
+        while (respuesta == 0 || arriba == true) {
+            // PASO 2. USUARIO INTRODUCE NUMERO
+            String consola = JOptionPane.showInputDialog(null,"¡Vamos a jugar! Tienes que adivinar un número entero entre 0 y 100 \n\nIntroduce un número entero");
+            // PASO 3 y 4. COMPROBAMOS VALIDEZ DEL NUMERO INTRODUCIDO
+            try {
+                    //sigue adelante si el número es entero, sino se va al catch
+                int intento = Integer.parseInt(consola);
+                    // se sale al acertar, al inicio comprobamos si está en rango y si quiere seguir jugando (una vez acertado)
+                while (intento <= 100 && intento >= 0 && respuesta == 0 && intento != numero) {
+                       // PASO 5. COMPROBAMOS SI ES MENOR QUE EL RANDOM
+                    if (intento < numero) {
+                        JOptionPane.showMessageDialog(null, "¡Lo siento! Te has quedado corto, inténtalo de nuevo con un número mayor. \nEste ha sido tu intento número " + contador);
+                        intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
+                    }
+                    else {  // PASO 6. SINO, ES QUE ES MAYOR
                         JOptionPane.showMessageDialog(null, "¡Lo siento! Te has pasado de largo, inténtalo de nuevo con un número menor. \nEste ha sido tu intento número " + contador);
                         intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce otro número (recuerda, entre 0 y 100)"));
                     }
-                contador= contador +1;
-
-                while(intento>100 || intento<0) { //comprobamos si el valor esté dentro del rango
-                    JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100.");
-                    intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
-
+                    contador= contador +1;
                 }
+                    // está fuera del while, pq ha acertado o pq está fuera de rango, hay que comprobarlo!
+                if (intento == numero) {
+                        // si ha acertado, lo informa y pregunta si quiere continuar, eligiendo entre 2 opciones
+                    JOptionPane.showMessageDialog(null, "¡Has acertado! El número aleatorio es: " + numero + "\nY lo has conseguido en tan solo " + contador + " intentos");
+                    contador = +1;  // reinicia el contador para volver a empezar, se pone a 0, pq aquí abajo le suma 1
+                        // pregunta si quiere seguir y dependiendo de la respuesta, vuelve arriba o sale del bucle
+                    Object[] opciones = {"Sí", "No"};
+                    respuesta = JOptionPane.showOptionDialog(null, "¿Quieres volver a jugar?", "Pregunta", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
+                    if (respuesta == 0) {
+                        arriba = true;  // SI = vuelve arriba
+                    }
+                    else {
+                        arriba = false;  // NO = se sale del bucle
+                    }
+                }
+                else {  // si no ha acertado, significa que está fuera de rango, así que informa y pide nuevo número
+                    JOptionPane.showMessageDialog(null, "Recuerda que los valores deben de estar entre 0 y 100. \nEste ha sido tu intento número " + contador);
+                    // intento = Integer.parseInt(JOptionPane.showInputDialog(null, "Introduce un número (recuerda, entre 0 y 100)"));
+                    contador= contador +1;
+                    arriba = true; // para volver arriba
+                }
+            } catch (NumberFormatException e) { //si está aquí es pq el número no es entero
+                    // se informa de ello y se pregunta si quiere volver a jugar
+                JOptionPane.showMessageDialog(null, "¡¡ No me engañas !! ¡ Ese no es un número entero ! \nEste ha sido tu intento número " + contador);
+                //contador = contador +1; **************** no sé pq pero si lo uso, me suma de dos en dos cuando no son enteros ********
+                contador= contador +1;
+                arriba = true;
             }
-                JOptionPane.showMessageDialog(null, "¡Has acertado! El número aleatorio es: " + numero + "\nY lo has conseguido en tan solo " + contador + " intentos");
-                
-                respuesta = JOptionPane.showOptionDialog(null, "¿Quieres volver a jugar?", "Pregunta", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, opciones[0]);
-        }
-
-        else {
-        }
-        
-    JOptionPane.showMessageDialog(null, "¡De acuerdo! Gracias por participar.");
+        } // salida del primer bucle
+            // si dice que no quiere seguir....
+        JOptionPane.showMessageDialog(null, "¡De acuerdo! Gracias por participar");
     }
-
-
-
-
-
-    sc.close();
-
-}
 }
 
-// OJO PROBAR A QUITAR EL IF...ELSE DE DESPUES DE RESPONDER SI A SEGUIR JUGANDO
+
+
+
